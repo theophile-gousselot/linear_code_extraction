9426b458c410824aa6ba578af6b09a7508116dc2
anonymous-submission002 <anonymous.submission002@gmail.com>
2022-10-25 15:32:20 +0200

    add already computed clock cycle overheads plot and don't remove them

--- functional_simulation/scripts/clean_everything.sh
+++ functional_simulation/scripts/clean_everything.sh
@@ -1,3 +1,3 @@
 find ../log/ -maxdepth 1 -type f -exec rm -v {} \;
-find ../log/plot/ -maxdepth 1 -type f -exec rm -v {} \;
+find ../log/plot/ -maxdepth 1 -type f \( ! -iname "plot_overheads_and_wwdls_4-35__average.pdf" ! -iname "plot_overheads_and_wwdls_4-35.pdf" \) -exec rm -v {} \;
 make -C ../core-v-verif/cv32e40p/sim/core/ clean_all
